ffde3ed0619608fa8c6653d8363e1d037f75d9a0
bentlyedyson <56628774+bentlyedyson@users.noreply.github.com>
2020-04-30 23:36:57 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,54 @@
+
+public class Shape {
+
+	private boolean filled;
+	   private String color;
+
+	   public Shape() {
+	      filled = true;
+	      color = "red";
+	   }
+
+	   public Shape(String colorIn, boolean filledIn) {
+	       filled = filledIn;
+	      color = colorIn;
+	   }
+
+
+	   public String getColor() {
+	     return color;
+	   }
+
+
+	   public void setColor(String colorSetIn) {
+	      color = colorSetIn;
+	   }
+
+	     
+	   public boolean isFilled()
+	   {
+	     if (filled == true)
+	     {
+	         return true;
+	     }
+	     else
+	     {
+	         return false;
+	     }
+	   }
+	
+	   public void setFilled(boolean filledSetIn) {
+	      filled = filledSetIn;
+	   }
+
+	
+	   public String toString()
+	   {
+	       String isNot = "";
+	       if(isFilled() == false)
+	       {
+	           isNot = "not ";
+	        }
+	           return "A Shape with color of " + color + " and is " + isNot + " filled. ";
+	        }
+	   }
